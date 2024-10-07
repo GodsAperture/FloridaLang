059fe07fde08fcaf563e6227facb1d78762ef5ae
GodsAperture <GodsAperture@yahoo.com>
2024-10-07 18:59:14 -0400

    New parser, thanks @munkeeBoi for the help.

--- Parser/Parser.cpp
+++ Parser/Parser.cpp
@@ -16,322 +16,42 @@ bool Parser::parse(){
 };
 
 Node* Parser::p0(){
+    Node* left = p1();
+    std::string current = given[iter].getName();
 
-    Node* thisNode;
-
-    thisNode = add();
-    if(thisNode != nullptr){
-        return thisNode;
+    while(current == "+" || current == "-"){
+        iter++;
+        Node* right = p1();
+        if(current == "+"){
+            left = stack->alloc<Add>(left, right);
+        } else {
+            left = stack->alloc<Subtract>(left, right);
+        }
+        current = given[iter].getName();
     }
 
-    thisNode = subtract();
-    if(thisNode != nullptr){
-        return thisNode;
-    }
-
-    thisNode = p1();
-    if(thisNode != nullptr){
-        return thisNode;
-    }
-
-    return nullptr;
+    return left;    
 }
 
 Node* Parser::p1(){
-    Node* thisNode = nullptr;
+    Node* left = p2();
+    std::string current = given[iter].getName();
 
-    thisNode = multiply();
-    if(thisNode != nullptr){
-        return thisNode;
+    while(current == "*" || current == "/"){
+        iter++;
+        Node* right = p2();
+        if(current == "*"){
+            left = stack->alloc<Multiply>(left, right);
+        } else {
+            left = stack->alloc<Divide>(left, right);
+        }   
+        current = given[iter].getName();
     }
 
-    thisNode = divide();
-    if(thisNode != nullptr){
-        return thisNode;
-    }
-
-    thisNode = p2();
-    if(thisNode != nullptr){
-        return thisNode;
-    }
-
-    return nullptr;
+    return left;
 }
 
 Node* Parser::p2(){
-    Node* thisNode = nullptr;
-
-    thisNode = parentheses();
-    if(thisNode != nullptr){
-        return thisNode;
-    }
-
-    // thisNode = brackets();
-    // if(thisNode != nullptr){
-    //     return thisNode;
-    // }
-
-    // thisNode = curly();
-    // if(thisNode != nullptr){
-    //     return thisNode;
-    // }
-
-    thisNode = p3();
-    if(thisNode != nullptr){
-        return thisNode;
-    }
-
-    return nullptr;
-}
-
-Node* Parser::p3(){
-    if(given[iter].getType() != FloridaType::fix8){
-        return nullptr;
-    }
-
     iter++;
     return stack->alloc<fixed64>(given[iter - 1].getName());
 }
-
-
-//Priority 0
-Node* Parser::add(){
-    const uint64_t initial = iter;
-    Add* thisNode = stack->alloc<Add>(nullptr, nullptr);
-
-    //Check if this program has reached the end of the token stream.
-    if((int64_t) (given.size() - iter) <= 2){
-        goto fail;
-    }
-
-    thisNode->left = p1();
-    //Check if left is a valid subexpression.
-    if(thisNode->left == nullptr){
-        goto fail;
-    }
-
-    //Check if the center of this subexpression is the "+" operator.
-    if(given[iter].getName() != "+"){
-        goto fail;
-    }
-    iter++;
-
-    if(given.size() <= iter){
-        goto fail;
-    }
-
-    thisNode->right = p0();
-    //Check if right is a valid subexpression.
-    if(thisNode->right == nullptr){
-        goto fail;
-    }
-
-    return thisNode;
-
-fail:
-    iter = initial;
-    stack->dealloc<Add>(thisNode);
-    return nullptr;
-}
-
-Node* Parser::subtract(){
-    const uint64_t initial = iter;
-    Subtract* thisNode = stack->alloc<Subtract>(nullptr, nullptr);
-
-    //Check if this program has reached the end of the token stream.
-    if((int64_t) (given.size() - iter) <= 2){
-        goto fail;
-    }
-
-    thisNode->left = p1();
-    //Check if left is a valid subexpression.
-    if(thisNode->left == nullptr){
-        goto fail;
-    }
-
-    //Check if the center of this subexpression is the "+" operator.
-    if(given[iter].getName() != "-"){
-        goto fail;
-    }
-    iter++;
-
-    thisNode->right = p0();
-    //Check if right is a valid subexpression.
-    if(thisNode->right == nullptr){
-        goto fail;
-    }
-
-    return thisNode;
-fail:
-    iter = initial;
-    stack->dealloc<Subtract>(thisNode);
-    return nullptr;
-}
-
-
-//Priority 1
-Node* Parser::multiply(){
-    const uint64_t initial = iter;
-    Multiply* thisNode = stack->alloc<Multiply>(nullptr, nullptr);
-
-    //Check if this program has reached the end of the token stream.
-    if((int64_t) (given.size() - iter) <= 2){
-        goto fail;
-    }
-
-    thisNode->left = p2();
-    //Check if left is a valid subexpression.
-    if(thisNode->left == nullptr){
-        goto fail;
-    }
-
-    //Check if the center of this subexpression is the "*" operator.
-    if(given[iter].getName() != "*"){
-        goto fail;
-    }
-    iter++;
-
-    if(given.size() <= iter){
-        goto fail;
-    }
-
-    thisNode->right = p1();
-    //Check if right is a valid subexpression.
-    if(thisNode->right == nullptr){
-        goto fail;
-    }
-
-
-    return thisNode;
-fail:
-    iter = initial;
-    stack->dealloc<Multiply>(thisNode);
-    return nullptr;
-}
-
-Node* Parser::divide(){
-    const uint64_t initial = iter;
-    Divide* thisNode = stack->alloc<Divide>(nullptr, nullptr);
-
-    //Check if this program has reached the end of the token stream.
-    if((int64_t) (given.size() - iter) <= 2)
-        goto fail;
-
-    thisNode->left = p2();
-    //Check if left is a valid subexpression.
-    if(thisNode->left == nullptr){
-        goto fail;
-    }
-
-    //Check if the center of this subexpression is the "+" operator.
-    if(given[iter].getName() != "/"){
-        goto fail;
-    }
-    iter++;
-
-    thisNode->right = p1();
-    //Check if right is a valid subexpression.
-    if(thisNode->right == nullptr){
-        goto fail;
-    }
-
-    return thisNode;
-fail:
-    iter = initial;
-    stack->dealloc<Divide>(thisNode);
-    return nullptr;
-}
-
-//Priority 2
-Node* Parser::parentheses(){
-    const uint64_t initial = iter;
-    Parnetheses* thisNode = stack->alloc<Parnetheses>(nullptr);
-
-    //Check if this program has reached the end of the token stream.
-    if((int64_t) (given.size() - iter) <= 2)
-        goto fail;
-
-    //Check if the center of this subexpression is the "+" operator.
-    if(given[iter].getName() != "("){
-        goto fail;
-    }
-    iter++;
-
-    thisNode->subexpression = p0();
-    //Check if left is a valid subexpression.
-    if(!p0()){
-        goto fail;
-    }
-
-    //Check if right is a valid subexpression.
-    if(given[iter].getName() != ")"){
-        goto fail;
-    }
-    iter++;
-
-    return thisNode;
-fail:
-    iter = initial;
-    stack->dealloc<Parnetheses>(thisNode);
-    return nullptr;
-}
-
-// bool Parser::brackets(){
-//     const uint64_t initial = iter;
-//     //Check if this program has reached the end of the token stream.
-//     if(given.size() - 1 <= iter){
-//         goto fail;
-//     }
-
-//     //Check if the center of this subexpression is the "+" operator.
-//     if(given[iter].getName() != "["){
-//         goto fail;
-//     }
-//     iter++;
-
-//     //Check if left is a valid subexpression.
-//     if(!p0()){
-//         goto fail;
-//     }
-
-//     //Check if right is a valid subexpression.
-//     if(given[iter].getName() != "]"){
-//         goto fail;
-//     }
-
-//     return true;
-
-// fail:
-//     iter = initial;
-//     return false;
-// }
-
-// bool Parser::curly(){
-//     const uint64_t initial = iter;
-//     //Check if this program has reached the end of the token stream.
-//     if(given.size() - 1 <= iter){
-//         goto fail;
-//     }
-
-//     //Check if the center of this subexpression is the "+" operator.
-//     if(given[iter].getName() != "{"){
-//         goto fail;
-//     }
-//     iter++;
-
-//     //Check if left is a valid subexpression.
-//     if(!p0()){
-//         goto fail;
-//     }
-
-//     //Check if right is a valid subexpression.
-//     if(given[iter].getName() != "}"){
-//         goto fail;
-//     }
-
-//     return true;
-
-// fail:
-//     iter = initial;
-//     return false;
-// }
